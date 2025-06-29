fe170b2074ab62a2f9b7b3b77217cd4aa324e2ff
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-06-29 19:51:01 +0200

    feat: Initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}", // Para TypeScript + React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
