888a22f57387674e3b11bec05d01816eb2ac4e95
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-07-03 17:51:09 +0200

    feat: Add create Table logic

---
+++ src/lib/forms/table/createTable.ts
@@ -0,0 +1,44 @@
+import type { TableDTO } from "@/model/table/tableDTO";
+import { toast } from "sonner";
+
+export default async function createTable(formData: FormData) {
+  const dashboardId = formData.get("dashboard_id")!.toString();
+  const tableDTO: TableDTO = {
+    name: formData.get("name")?.toString(),
+    cardList: []
+  };
+
+  const createTableRes = await fetch("http://localhost:8080/api/tables", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(tableDTO),
+  });
+
+  if (!createTableRes.ok) {
+    console.error("Error creating table:", await createTableRes.text());
+    toast.error(`Error creating table: ${await createTableRes.text()}`);
+    throw Error(`Error creating table: ${await createTableRes.text()}`)
+  }
+
+  const createdTable = await createTableRes.json();
+  const tableId = createdTable.id;
+
+  const addTableRes = await fetch(`http://localhost:8080/api/dashboards/${dashboardId}/tables`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(tableId),
+  });
+
+  if (!addTableRes.ok) {
+    console.error("Error adding table:", await addTableRes.text(), " to dashboard:", dashboardId);
+    toast.error(`Error adding table: ${await addTableRes.text()}`);
+    throw Error(`Error adding table: ${await addTableRes.text()} to dashboard: ${dashboardId}`)
+  }
+
+  console.log("Table created successfully");
+  toast.success("Table has been created.");
+}
